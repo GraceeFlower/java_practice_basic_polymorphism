43aac08c78cd6aaac54c70f41302f2954dc8974d
GraceeFlower <1491644103@qq.com>
2020-02-10 16:03:13 +0800

    [WM] feat: print out the different apps' pressF5-behavior

--- 006_polymorphism/src/main/java/Application.java
+++ 006_polymorphism/src/main/java/Application.java
@@ -1,5 +1,14 @@
 public class Application {
     public static void main(String[] args) {
+        WindowsApp intelliJ = new IntelliJ();
+        WindowsApp chorme = new Chrome();
+        WindowsApp weChat = new WeChat();
 
+        System.out.println("- IntelliJ IDEA -");
+        intelliJ.pressF5();
+        System.out.println("- Chrome -");
+        chorme.pressF5();
+        System.out.println("- WeChat -");
+        weChat.pressF5();
     }
 }
